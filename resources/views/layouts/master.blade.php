40501ccea3e53713a7db034a9d464332013c92ee
Gerald Plakasa <spanixgerald@gmail.com>
2020-07-09 08:07:04 +0700

    edit bagian master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title>CodeForumIDN</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -31,7 +31,7 @@
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">CodeForumIDN</span>
     </a>
 
     <!-- Sidebar -->
